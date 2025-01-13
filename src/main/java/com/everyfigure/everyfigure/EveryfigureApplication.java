30bf0a68982a0d656817466fa3983d9b24f61e06
ChuHyunSik <inin7674@naver.com>
2025-01-13 15:23:59 +0900

    Initial commit

---
+++ src/main/java/com/everyfigure/everyfigure/EveryfigureApplication.java
@@ -0,0 +1,13 @@
+package com.everyfigure.everyfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryfigureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everyfigure/everyfigure/EveryfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyfigure.everyfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryfigureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
